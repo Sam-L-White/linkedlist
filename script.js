7a005790fe99b65ccedd4b1bc1bca2d5542a5f66
Sam <98746718+Sam-L-White@users.noreply.github.com>
2023-03-24 12:46:26 +0000

    Add list size, fix append if/else

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 const linkedList = () => {
     let head = null
+    let size = 0
 
     const prepend = (value) =>{
         head = node(value, head)
-        console.log(head)
+        size ++
     }
 
     const append = (value) =>{
         if(head === null){
             head = node(value)
+            size++
+        } else {
+            let pointer = head
+            while(pointer.nextNode !== null){
+                pointer = pointer.nextNode
+            }
+            pointer.nextNode = node(value)
+            size++
         }
-        let pointer = head
-        while(pointer.nextNode !== null){
-            pointer = pointer.nextNode
-        }
-        pointer.nextNode = node(value)
     }
 
-    return{prepend, append, get head(){return head}}
+    return{prepend, append, get head(){return head}, get size(){return size}}
 }
 
 const node = (value = null, nextNode = null) => {
@@ -28,3 +32,4 @@ let list = linkedList()
 list.prepend(2)
 list.prepend(4)
 list.append(6)
+console.log(list.size)
